ed99745dc4d391d5641e7e9ccd6600442e415110
Anup Rawat <anuprawat077@gmail.com>
2023-07-29 13:33:23 +0530

    Initial Commit

---
+++ web-application/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> Freelancing Website</header>
+      <p>let start new work</p>
+    </div>
+  );
+}
+
+export default App;
